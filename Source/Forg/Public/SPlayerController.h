2891666a8e7a9ad4a152e80ff88e90946dae4f88
Ian Farnan <IFarnan90@outlook.com>
2022-07-07 12:52:17 +0100

    - Menu Lobby Joining via IP. In-game menu widget to quit, return to menu etc
    - Setup in-game widget to quit game and return to menu
    - New PlayerController class with pause functions

---
+++ Source/Forg/Public/SPlayerController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORG_API ASPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePauseMenu();
+
+	void SetupInputComponent() override;
+	
+};
